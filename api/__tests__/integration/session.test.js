a9f8b0dbc2e2f3fc3f432894dbc0f7028d5bd3e1
marcos <marcos.barboza@contaemdia.com.vc>
2019-06-09 23:57:32 -0300

    adicionando teste de criação de usuario

--- api/__tests__/integration/session.test.js
+++ api/__tests__/integration/session.test.js
@@ -1,15 +1,25 @@
+const request = require("supertest");
+const app = require("../../src/app");
 const usuario = require("../../src/models/usuario");
 
 describe("Register", () => {
   it("User", async () => {
     const user = await usuario.create({
       nome: "marcos",
-      email: "leandro@gmail.com",
-      senha: "123456",
+      email: "leandrobarboza2@gmail.com",
+      senha: "12345678",
       dataNascimento: "24061997",
       telefone: "998166870"
     });
-    console.log(user);
-    expected(user.email).toBe("leandro@gmail.com");
+    const response = await request(app)
+      .post("/cadastrar")
+      .send({
+        nome: user.nome,
+        email: user.email,
+        senha: user.senha,
+        dataNascimento: user.dataNascimento,
+        telefone: user.telefone
+      });
+    expect(response.status).toBe(200);
   });
 });
